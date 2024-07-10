f15b07e55f2f449002495462d4a370839cc3e727
abhay gupta <abhaykumargupta428@gmail.com>
2024-07-10 19:31:47 +0530

    code pushed to git

--- app/src/main/java/com/newsbytes/abhay/ui/DetailsActivity.kt
+++ app/src/main/java/com/newsbytes/abhay/ui/DetailsActivity.kt
@@ -1,10 +1,12 @@
 package com.newsbytes.abhay.ui
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.newsbytes.abhay.BuildConfig
 import com.newsbytes.abhay.R
 import com.newsbytes.abhay.databinding.ActivityDetailsBinding
 import com.newsbytes.abhay.jetpack.bases.BaseActivity
@@ -15,7 +17,11 @@ class DetailsActivity : BaseActivity() {
 
     private lateinit var binding: ActivityDetailsBinding
     private val model by lazy {
-        intent.getParcelableExtra<RecipeEntity>(IntentKeys.INTENT_FOR_DATA)!!
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra(IntentKeys.INTENT_FOR_DATA, RecipeEntity::class.java)
+        } else {
+            intent.getParcelableExtra(IntentKeys.INTENT_FOR_DATA)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +37,7 @@ class DetailsActivity : BaseActivity() {
         binding.idRecyclerView.layoutManager = LinearLayoutManager(this)
         val adapter = StringAdapter()
         binding.idRecyclerView.adapter = adapter
-        adapter.notifyAdapterDataSetChanged(modelList = model.ingredients)
+        adapter.notifyAdapterDataSetChanged(modelList = model?.ingredients ?: arrayListOf())
     }
 
 
@@ -45,9 +51,9 @@ class DetailsActivity : BaseActivity() {
     override fun initializeData() {
         super.initializeData()
         binding.apply {
-            idTitle.text = model.title
-            idTitleDescription.text = model.description
-            Glide.with(this@DetailsActivity).load(model.image).centerCrop()
+            idTitle.text = model?.title
+            idTitleDescription.text = model?.description
+            Glide.with(this@DetailsActivity).load(model?.image).centerCrop()
                 .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
                 .placeholder(R.drawable.placeholder_round).into(idImage)
         }
