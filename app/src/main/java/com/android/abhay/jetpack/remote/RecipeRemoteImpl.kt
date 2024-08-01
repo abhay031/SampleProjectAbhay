0d2d3498c4693b3246ef4183f9703d05e5dc08e7
abhay gupta <abhaykumargupta428@gmail.com>
2024-08-01 17:46:16 +0530

    code updated

--- app/src/main/java/com/android/abhay/jetpack/remote/RecipeRemoteImpl.kt
+++ app/src/main/java/com/android/abhay/jetpack/remote/RecipeRemoteImpl.kt
@@ -1,23 +1,22 @@
-package  com.newsbytes.abhay.jetpack.remote
+package  com.android.abhay.jetpack.remote
 
-import com.google.gson.JsonObject
-import com.newsbytes.abhay.jetpack.bases.OnEventTriggerListener
-import com.newsbytes.abhay.jetpack.bases.ParseJson
-import com.newsbytes.abhay.jetpack.bases.ResponseType
-import com.newsbytes.abhay.jetpack.bases.Urls
-import com.newsbytes.abhay.jetpack.entities.RecipeEntity
-import com.newsbytes.abhay.jetpack.retrofit.RetrofitUtil
-import com.newsbytes.abhay.utils.Constants.KEY_UNAUTHORISED_CODE
+import com.android.abhay.jetpack.bases.OnEventTriggerListener
+import com.android.abhay.jetpack.bases.ParseJson
+import com.android.abhay.jetpack.bases.ResponseType
+import com.android.abhay.jetpack.bases.Urls
+import com.android.abhay.jetpack.entities.RecipeEntity
+import com.android.abhay.jetpack.retrofit.RetrofitUtil
+import com.android.abhay.utils.Constants.KEY_UNAUTHORISED_CODE
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-class RecipeRemote {
+class RecipeRemoteImpl : RecipeRemote {
 
     private var apiCall: Call<Any>? = null
 
-    fun apiGetData(
+    override fun apiGetData(
         listener: OnEventTriggerListener
     ) {
         apiCall?.cancel()
@@ -49,7 +48,7 @@ class RecipeRemote {
     }
 
 
-    fun closeCall() {
+    override fun closeCall() {
         apiCall?.cancel()
     }
 
